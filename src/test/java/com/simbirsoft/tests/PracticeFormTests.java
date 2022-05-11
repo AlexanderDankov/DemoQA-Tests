0493906ec5848245e51fd4c1a62f39c92acedc26
AleksandrDankov <dankoffalexander@gmail.com>
2022-05-11 21:59:28 +0500

    Changed PracticeFormTests class: added locators for filling form

--- src/test/java/com/simbirsoft/tests/PracticeFormTests.java
+++ src/test/java/com/simbirsoft/tests/PracticeFormTests.java
@@ -1,6 +1,7 @@
 package com.simbirsoft.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class PracticeFormTests {
     @BeforeAll
     static void beforeAll() {
+
         Configuration.startMaximized = true;
     }
 
@@ -19,11 +21,23 @@ public class PracticeFormTests {
         String firstName = "Alexander";
         String lastName = "Dankov";
         String email = "dankoffalexander@gmail.com";
+        String phoneNumber = "89507230992";
+        String subject = "Maths";
 
         open("https://demoqa.com/automation-practice-form");
         $("input#firstName").setValue(firstName);
         $("input#lastName").setValue(lastName);
         $("input#userEmail").setValue(email);
+        $("#genterWrapper").$("[for=gender-radio-1]").click();
+        $("input#userNumber").setValue(phoneNumber);
+        $("input#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("5");
+        $(".react-datepicker__year-select").selectOptionByValue("1991");
+        $(".react-datepicker__week").$(new Selectors.ByText("30")).click();
+        $("input#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$("[for=hobbies-checkbox-1]").click();
+        $("input#uploadPicture").uploadFile()
+
         $("#submit").click();
 
 
