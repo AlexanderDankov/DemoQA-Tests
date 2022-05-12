b020f3341380882fab4b84d6a696f756e049dea2
AleksandrDankov <dankoffalexander@gmail.com>
2022-05-12 23:44:55 +0500

    Update configuration to use firefox browser Added tests for PracticeFormTests

--- src/test/java/com/simbirsoft/tests/PracticeFormTests.java
+++ src/test/java/com/simbirsoft/tests/PracticeFormTests.java
@@ -4,16 +4,18 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
     @BeforeAll
     static void beforeAll() {
 
         Configuration.startMaximized = true;
+        Configuration.browser = "firefox";
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
@@ -21,11 +23,11 @@ public class PracticeFormTests {
         String firstName = "Alexander";
         String lastName = "Dankov";
         String email = "dankoffalexander@gmail.com";
-        String phoneNumber = "89507230992";
+        String phoneNumber = "8950723099";
         String subject = "Maths";
         String currentAddress = "Lenina str. 1";
 
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
         $("input#firstName").setValue(firstName);
         $("input#lastName").setValue(lastName);
         $("input#userEmail").setValue(email);
@@ -38,9 +40,23 @@ public class PracticeFormTests {
         $("input#subjectsInput").setValue(subject).pressEnter();
         $("#hobbiesWrapper").$("[for=hobbies-checkbox-1]").click();
         $("input#uploadPicture").uploadFromClasspath("img.png");
-
+        $("textarea#currentAddress").setValue(currentAddress);
+        $("#state").scrollIntoView(true).click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
         $("#submit").click();
 
+        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName));
+        $x("//td[text()='Student Email']").parent().shouldHave(text(email));
+        $x("//td[text()='Gender']").parent().shouldHave(text("Male"));
+        $x("//td[text()='Mobile']").parent().shouldHave(text(phoneNumber));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text("30 May,1991"));
+        $x("//td[text()='Subjects']").parent().shouldHave(text(subject));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text("Sports"));
+        $x("//td[text()='Picture']").parent().shouldHave(text("img.png"));
+        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+        $x("//td[text()='State and City']").parent().shouldHave(text("Haryana Karnal"));
 
     }
 }
