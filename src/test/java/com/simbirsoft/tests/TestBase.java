7f36402f5d4f00d12fcbe43c2ab0a1273b714197
AleksandrDankov <dankoffalexander@gmail.com>
2022-07-22 12:02:01 +0500

    Update build, update test base, created attaches

--- src/test/java/com/simbirsoft/tests/TestBase.java
+++ src/test/java/com/simbirsoft/tests/TestBase.java
@@ -1,13 +1,33 @@
 package com.simbirsoft.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.simbirsoft.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
-    static void beforeAll() {
+    static void setup() {
         Configuration.startMaximized = true;
-        Configuration.browser = "firefox";
         Configuration.baseUrl = "https://demoqa.com";
+
+        SelenideLogger.addListener("AllureListener", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
